e96be8e827fc26585ed8b1d3bb3348b16f273be9
Nitin Singh Naruka <qualdev.nitinsingh@gmaile.com>
2023-01-28 15:05:50 +0530

    Change login icon

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,4 +1,4 @@
-import { LockClosedIcon } from '@heroicons/react/20/solid'
+import { HiLockClosed } from 'react-icons/hi2'
 
 const Login = () => {
   return (
@@ -89,7 +89,7 @@ const Login = () => {
                 className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                  <LockClosedIcon
+                  <HiLockClosed
                     className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
                     aria-hidden="true"
                   />
